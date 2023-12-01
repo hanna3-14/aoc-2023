c417ffc5d9763448d5176f75fa76ce144d21a8b8
hanna3-14 <114485436+hanna3-14@users.noreply.github.com>
2023-12-01 11:50:54 +0100

    day1 part2 beautiful

--- src/Day01.kt
+++ src/Day01.kt
@@ -16,51 +16,45 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        var digits = emptyArray<String>()
         var solve = ""
         var sum = 0
-        var numbers = listOf("one", "two","three","four","five","six","seven","eight","nine", "1", "2", "3", "4", "5", "6", "7", "8", "9")
-        var haha: Pair<Int, String>
-        var lel: Pair<Int, String>
+        var numbers = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+        var firstDigit: Pair<Int, String>
+        var lastDigit: Pair<Int, String>
 
-        for (i in input){
-            haha = i.findAnyOf(numbers)!!
-            lel = i.findLastAnyOf(numbers)!!
-            when(haha.second){
-                "one", "1" -> solve += 1
-                "two", "2" -> solve += 2
-                "three", "3" -> solve += 3
-                "four", "4" -> solve += 4
-                "five", "5" -> solve += 5
-                "six", "6" -> solve += 6
-                "seven", "7" -> solve += 7
-                "eight", "8" -> solve += 8
-                "nine", "9" -> solve += 9
-            }
-            when(lel.second){
-                "one", "1" -> solve += 1
-                "two", "2" -> solve += 2
-                "three", "3" -> solve += 3
-                "four", "4" -> solve += 4
-                "five", "5" -> solve += 5
-                "six", "6" -> solve += 6
-                "seven", "7" -> solve += 7
-                "eight", "8" -> solve += 8
-                "nine", "9" -> solve += 9
-            }
-            // println(solve)
+        input.forEach { s: String ->
+            firstDigit = s.findAnyOf(numbers)!!
+            lastDigit = s.findLastAnyOf(numbers)!!
+            solve += valueOfString(firstDigit.second)
+            solve += valueOfString(lastDigit.second)
             sum += solve.toInt()
             solve = ""
         }
-        println(sum)
-        return input.size
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_1_test")
     check(part1(testInput) == 142)
+    val testInput2 = readInput("Day01_2_test")
+    check(part2(testInput2) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+}
+
+fun valueOfString(string: String): Int {
+    return when (string) {
+        "one" -> 1
+        "two" -> 2
+        "three" -> 3
+        "four" -> 4
+        "five" -> 5
+        "six" -> 6
+        "seven" -> 7
+        "eight" -> 8
+        "nine" -> 9
+        else -> string.toInt()
+    }
 }
