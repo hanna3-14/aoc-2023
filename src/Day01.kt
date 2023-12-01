fbd206b7ce142407b3100e013ae7f0357658ec2c
hanna3-14 <114485436+hanna3-14@users.noreply.github.com>
2023-12-01 11:11:12 +0100

    day1 ugly

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,68 @@
 fun main() {
+
     fun part1(input: List<String>): Int {
+
+        var digits = emptyArray<String>()
+        var solve = ""
+        var sum = 0
+
+        for (i in input){
+            digits = arrayOf(i.filter { it.isDigit() })
+            for (d in digits){
+                solve += digits.first().first()
+                solve += digits.first().last()
+                sum += solve.toInt()
+                solve = ""
+            }
+        }
+        println(sum)
         return input.size
     }
 
     fun part2(input: List<String>): Int {
+        var digits = emptyArray<String>()
+        var solve = ""
+        var sum = 0
+        var numbers = listOf("one", "two","three","four","five","six","seven","eight","nine", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+        var haha: Pair<Int, String>
+        var lel: Pair<Int, String>
+
+        for (i in input){
+            haha = i.findAnyOf(numbers)!!
+            lel = i.findLastAnyOf(numbers)!!
+            when(haha.second){
+                "one", "1" -> solve += 1
+                "two", "2" -> solve += 2
+                "three", "3" -> solve += 3
+                "four", "4" -> solve += 4
+                "five", "5" -> solve += 5
+                "six", "6" -> solve += 6
+                "seven", "7" -> solve += 7
+                "eight", "8" -> solve += 8
+                "nine", "9" -> solve += 9
+            }
+            when(lel.second){
+                "one", "1" -> solve += 1
+                "two", "2" -> solve += 2
+                "three", "3" -> solve += 3
+                "four", "4" -> solve += 4
+                "five", "5" -> solve += 5
+                "six", "6" -> solve += 6
+                "seven", "7" -> solve += 7
+                "eight", "8" -> solve += 8
+                "nine", "9" -> solve += 9
+            }
+            // println(solve)
+            sum += solve.toInt()
+            solve = ""
+        }
+        println(sum)
         return input.size
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    // val testInput = readInput("Day01_test")
+    // check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     part1(input).println()
