2e504782af6e032909b1b0c035061cbd230d4752
hanna3-14 <114485436+hanna3-14@users.noreply.github.com>
2023-12-05 23:34:56 +0100

    concept for day5 part2

--- src/Day05.kt
+++ src/Day05.kt
@@ -53,21 +53,74 @@ fun main() {
         return locations.min()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        var location = 5000000000
+        var seeds = input[0].split(' ')
+        seeds = seeds.subList(1, seeds.size)
+        var seedsInt = mutableListOf<Long>()
+        seeds.forEach { seed: String ->
+            seedsInt.add(seed.toLong())
+        }
+        var seedChunks = seedsInt.chunked(2)
+        var indexes = mutableListOf<Int>()
+        input.forEachIndexed { index: Int, s: String ->
+            if (s.contains("map")) {
+                indexes += index + 1
+            }
+        }
+        var seedToSoil = input.subList(indexes[0], indexes[1] - 2)
+        var soilToFertilizer = input.subList(indexes[1], indexes[2] - 2)
+        var fertilizerToWater = input.subList(indexes[2], indexes[3] - 2)
+        var waterToLight = input.subList(indexes[3], indexes[4] - 2)
+        var lightToTemperature = input.subList(indexes[4], indexes[5] - 2)
+        var temperatureToHumidity = input.subList(indexes[5], indexes[6] - 2)
+        var humidityToLocation = input.subList(indexes[6], input.size)
+
+        var soil = 0.toLong()
+        for (chunk in seedChunks) {
+            for (i in chunk.first()..<chunk.first() + chunk.last()) {
+                var seed = i
+                soil = doImportantStuff(seedToSoil, seed)
+                var fertilizer = 0.toLong()
+                fertilizer = doImportantStuff(soilToFertilizer, soil)
+
+                var water = 0.toLong()
+                water = doImportantStuff(fertilizerToWater, fertilizer)
+
+                var light = 0.toLong()
+                light = doImportantStuff(waterToLight, water)
+
+                var temperature = 0.toLong()
+                temperature = doImportantStuff(lightToTemperature, light)
+
+                var humidity = 0.toLong()
+                humidity = doImportantStuff(temperatureToHumidity, temperature)
+
+                var result = 0.toLong()
+
+                result = doImportantStuff(humidityToLocation, humidity)
+
+                if (result < location) {
+                    location = result
+                }
+                println(location)
+            }
+            println(location)
+        }
+        return location
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_1_test")
-    check(part1(testInput) == 35.toLong())
-    // check(part2(testInput) == 30)
+    // check(part1(testInput) == 35.toLong())
+    check(part2(testInput) == 46.toLong())
 
     val input = readInput("Day05")
-    part1(input).println()
+    // part1(input).println()
     part2(input).println()
 
     // check after submitting the solution
-    check(part1(input) == 486613012.toLong())
+    // check(part1(input) == 486613012.toLong())
     // check(part2(input) == 5923918)
 }
 
