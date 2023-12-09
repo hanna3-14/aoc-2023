ea32ac98b1c292909964a7e4051d2d436c6ed359
hanna3-14 <114485436+hanna3-14@users.noreply.github.com>
2023-12-09 15:17:40 +0100

    day7 concept

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,8 +1,41 @@
 fun main() {
 
     fun part1(input: List<String>): Int {
+
+        var completelySortedList = mutableListOf<Int>()
+        var sum = 0
+
+        val customComparator = object : Comparator<String> {
+            override fun compare(o1: String, o2: String): Int {
+                // Custom logic for comparing letters
+                val order = mapOf(
+                    'A' to 14,
+                    'Q' to 13,
+                    'K' to 12,
+                    'J' to 11,
+                    'T' to 10,
+                    '9' to 9,
+                    '8' to 8,
+                    '7' to 7,
+                    '6' to 6,
+                    '5' to 5,
+                    '4' to 4,
+                    '3' to 3,
+                    '2' to 2
+                )
+
+                for (i in 0..o1.length) {
+                    val letter = order.getOrDefault(o2[i], -1) - order.getOrDefault(o1[i], -1)
+                    if (letter != 0) {
+                        return letter
+                    }
+                }
+                return 0
+            }
+        }
+
         val fives = mutableListOf<String>()
-        var fours = mutableListOf<String>()
+        val fours = mutableListOf<String>()
         val fullhouses = mutableListOf<String>()
         val threes = mutableListOf<String>()
         val twoPairs = mutableListOf<String>()
@@ -14,31 +47,73 @@ fun main() {
                 fives.add(s)
             } else if (isFour(chunks.first())) {
                 fours.add(s)
-            } else if(isFullHouse(chunks.first())) {
+            } else if (isFullHouse(chunks.first())) {
                 fullhouses.add(s)
-            } else if(isThree(chunks.first())){
+            } else if (isThree(chunks.first())) {
                 threes.add(s)
-            }else if (isTwoPair(chunks.first())){
+            } else if (isTwoPair(chunks.first())) {
                 twoPairs.add(s)
-            }else if(isOnePair(chunks.first())){
+            } else if (isOnePair(chunks.first())) {
                 onePairs.add(s)
-            }else{
+            } else {
                 highCards.add(s)
             }
         }
-        println(fives)
-        println(fours)
-       /* println(fullhouses)
-        println(threes)
-        println(twoPairs)
+
+        println(highCards)
         println(onePairs)
-        println(highCards)*/
-        //compareBy<Char>{ it.}
-        //fours = fours.sortedWith(compareBy<String>{ it.first()}.thenBy{ it[1]}.thenBy{it[2]}).toMutableList()
-        // fours.sort(Comparator.comparingChar(p ->))
-        //fours = fours.sortedWith(compareBy({ it.customProperty }))
+        println(twoPairs)
+        println(threes)
+        println(fullhouses)
         println(fours)
-        return input.size
+        println(fives)
+
+        val highCardsSorted = highCards.sortedWith(customComparator).reversed()
+        val onePairsSorted = onePairs.sortedWith(customComparator).reversed()
+        val twoPairsSorted = twoPairs.sortedWith(customComparator).reversed()
+        val threesSorted = threes.sortedWith(customComparator).reversed()
+        val fullhousesSorted = fullhouses.sortedWith(customComparator).reversed()
+        val foursSorted = fours.sortedWith(customComparator).reversed()
+        val fivesSorted = fives.sortedWith(customComparator).reversed()
+
+        println(highCardsSorted)
+        println(onePairsSorted)
+        println(twoPairsSorted)
+        println(threesSorted)
+        println(fullhousesSorted)
+        println(foursSorted)
+        println(fivesSorted)
+
+        highCardsSorted.forEach { s: String ->
+            completelySortedList.add(s.split(' ').last().toInt())
+        }
+        onePairsSorted.forEach { s: String ->
+            completelySortedList.add(s.split(' ').last().toInt())
+        }
+        twoPairsSorted.forEach { s: String ->
+            completelySortedList.add(s.split(' ').last().toInt())
+        }
+        threesSorted.forEach { s: String ->
+            completelySortedList.add(s.split(' ').last().toInt())
+        }
+        fullhousesSorted.forEach { s: String ->
+            completelySortedList.add(s.split(' ').last().toInt())
+        }
+        foursSorted.forEach { s: String ->
+            completelySortedList.add(s.split(' ').last().toInt())
+        }
+        fivesSorted.forEach { s: String ->
+            completelySortedList.add(s.split(' ').last().toInt())
+        }
+
+        println(completelySortedList)
+
+        completelySortedList.forEachIndexed { index, number ->
+            sum += (index + 1) * number
+            // println("index: ${index + 1}, number: $number")
+        }
+
+        return sum
     }
 
     fun part2(input: List<String>): Int {
@@ -47,11 +122,14 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day07_1_test")
-    // check(part1(testInput) == 6440)
+    check(part1(testInput) == 6440)
     // check(part2(testInput) == 71503)
 
     val input = readInput("Day07")
-    part1(input).println()
+    // part1(input).println()
+
+    // 251.142.166 too high
+    // 249.858.334 too low
     // part2(input).println()
 
     // check after submitting the solution
@@ -66,8 +144,8 @@ fun isFive(input: String): Boolean {
 fun isFour(input: String): Boolean {
     val validLetters = listOf('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2')
     val numbers = mutableListOf<Int>()
-    validLetters.forEach{letter: Char ->
-        numbers.add(input.count { it == letter})
+    validLetters.forEach { letter: Char ->
+        numbers.add(input.count { it == letter })
     }
     return numbers.contains(4)
 }
@@ -75,8 +153,8 @@ fun isFour(input: String): Boolean {
 fun isFullHouse(input: String): Boolean {
     val validLetters = listOf('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2')
     val numbers = mutableListOf<Int>()
-    validLetters.forEach{letter: Char ->
-        numbers.add(input.count { it == letter})
+    validLetters.forEach { letter: Char ->
+        numbers.add(input.count { it == letter })
     }
     return numbers.contains(3) && numbers.contains(2)
 }
@@ -84,8 +162,8 @@ fun isFullHouse(input: String): Boolean {
 fun isThree(input: String): Boolean {
     val validLetters = listOf('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2')
     val numbers = mutableListOf<Int>()
-    validLetters.forEach{letter: Char ->
-        numbers.add(input.count { it == letter})
+    validLetters.forEach { letter: Char ->
+        numbers.add(input.count { it == letter })
     }
     return numbers.contains(3)
 }
@@ -93,8 +171,8 @@ fun isThree(input: String): Boolean {
 fun isTwoPair(input: String): Boolean {
     val validLetters = listOf('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2')
     val numbers = mutableListOf<Int>()
-    validLetters.forEach{letter: Char ->
-        numbers.add(input.count { it == letter})
+    validLetters.forEach { letter: Char ->
+        numbers.add(input.count { it == letter })
     }
     return numbers.filter { it == 2 }.size == 2
 }
@@ -102,25 +180,8 @@ fun isTwoPair(input: String): Boolean {
 fun isOnePair(input: String): Boolean {
     val validLetters = listOf('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2')
     val numbers = mutableListOf<Int>()
-    validLetters.forEach{letter: Char ->
-        numbers.add(input.count { it == letter})
+    validLetters.forEach { letter: Char ->
+        numbers.add(input.count { it == letter })
     }
     return numbers.filter { it == 2 }.size == 1
-}
-
-/*fun sortList(list: MutableList<String>):MutableList<String>{
-    list.forEach { entry: String ->
-        var chars = entry.toCharArray()
-        println(chars)
-    }
-    return list
-}*/
-
-class CharComparator {
-    companion object : Comparator<Char> {
-        override fun compare(c1: Char, c2: Char): Int {
-            var numberToCharMap = mapOf('A' to 14, 'K' to 13, 'Q' to 12, 'J' to 11, 'T' to 10, '9' to 9, '8' to 8, '7' to 7, '6' to 6, '5' to 5, '4' to 4, '3' to 3, '2' to 2)
-            return numberToCharMap[c1]!! - numberToCharMap[c2]!!
-        }
-    }
 }
