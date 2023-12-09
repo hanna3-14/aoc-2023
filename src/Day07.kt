907fb2add07fa1c578b9da1bce0f3469e3c25d2f
hanna3-14 <114485436+hanna3-14@users.noreply.github.com>
2023-12-09 16:40:13 +0100

    day7 part2

--- src/Day07.kt
+++ src/Day07.kt
@@ -2,7 +2,7 @@ fun main() {
 
     fun part1(input: List<String>): Int {
 
-        var completelySortedList = mutableListOf<Int>()
+        val completelySortedList = mutableListOf<Int>()
         var sum = 0
 
         val customComparator = object : Comparator<String> {
@@ -98,21 +98,127 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+
+        val completelySortedList = mutableListOf<Int>()
+        var sum = 0
+
+        val customComparator = object : Comparator<String> {
+            override fun compare(o1: String, o2: String): Int {
+                // Custom logic for comparing letters
+                val order = mapOf(
+                    'A' to 13,
+                    'K' to 12,
+                    'Q' to 11,
+                    'T' to 10,
+                    '9' to 9,
+                    '8' to 8,
+                    '7' to 7,
+                    '6' to 6,
+                    '5' to 5,
+                    '4' to 4,
+                    '3' to 3,
+                    '2' to 2,
+                    'J' to 1
+                )
+
+                for (i in 0..o1.length) {
+                    val letter = order.getOrDefault(o2[i], -1) - order.getOrDefault(o1[i], -1)
+                    if (letter != 0) {
+                        return letter
+                    }
+                }
+                return 0
+            }
+        }
+
+        val fives = mutableListOf<String>()
+        val fours = mutableListOf<String>()
+        val fullhouses = mutableListOf<String>()
+        val threes = mutableListOf<String>()
+        val twoPairs = mutableListOf<String>()
+        val onePairs = mutableListOf<String>()
+        val highCards = mutableListOf<String>()
+        input.forEach { s: String ->
+            if (!s.contains('J')) {
+                val chunks = s.split(' ')
+                if (isFive(chunks.first())) {
+                    fives.add(s)
+                } else if (isFour(chunks.first())) {
+                    fours.add(s)
+                } else if (isFullHouse(chunks.first())) {
+                    fullhouses.add(s)
+                } else if (isThree(chunks.first())) {
+                    threes.add(s)
+                } else if (isTwoPair(chunks.first())) {
+                    twoPairs.add(s)
+                } else if (isOnePair(chunks.first())) {
+                    onePairs.add(s)
+                } else {
+                    highCards.add(s)
+                }
+            } else {
+                val categoryNumber = categorizeOneJoker(s)
+                when (categoryNumber) {
+                    0 -> highCards.add(s)
+                    1 -> onePairs.add(s)
+                    2 -> twoPairs.add(s)
+                    3 -> threes.add(s)
+                    4 -> fullhouses.add(s)
+                    5 -> fours.add(s)
+                    6 -> fives.add(s)
+                }
+            }
+        }
+
+        val highCardsSorted = highCards.sortedWith(customComparator).reversed()
+        val onePairsSorted = onePairs.sortedWith(customComparator).reversed()
+        val twoPairsSorted = twoPairs.sortedWith(customComparator).reversed()
+        val threesSorted = threes.sortedWith(customComparator).reversed()
+        val fullhousesSorted = fullhouses.sortedWith(customComparator).reversed()
+        val foursSorted = fours.sortedWith(customComparator).reversed()
+        val fivesSorted = fives.sortedWith(customComparator).reversed()
+
+        highCardsSorted.forEach { s: String ->
+            completelySortedList.add(s.split(' ').last().toInt())
+        }
+        onePairsSorted.forEach { s: String ->
+            completelySortedList.add(s.split(' ').last().toInt())
+        }
+        twoPairsSorted.forEach { s: String ->
+            completelySortedList.add(s.split(' ').last().toInt())
+        }
+        threesSorted.forEach { s: String ->
+            completelySortedList.add(s.split(' ').last().toInt())
+        }
+        fullhousesSorted.forEach { s: String ->
+            completelySortedList.add(s.split(' ').last().toInt())
+        }
+        foursSorted.forEach { s: String ->
+            completelySortedList.add(s.split(' ').last().toInt())
+        }
+        fivesSorted.forEach { s: String ->
+            completelySortedList.add(s.split(' ').last().toInt())
+        }
+
+        completelySortedList.forEachIndexed { index, number ->
+            sum += (index + 1) * number
+        }
+
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day07_1_test")
     check(part1(testInput) == 6440)
-    // check(part2(testInput) == 71503)
+    check(part2(testInput) == 5905)
 
     val input = readInput("Day07")
     part1(input).println()
-    // part2(input).println()
+    part2(input).println()
 
     // check after submitting the solution
     check(part1(input) == 250898830)
-    // check(part2(input) == 34278221)
+    check(part2(input) == 252127335)
 }
 
 fun isFive(input: String): Boolean {
@@ -162,4 +268,32 @@ fun isOnePair(input: String): Boolean {
         numbers.add(input.count { it == letter })
     }
     return numbers.filter { it == 2 }.size == 1
+}
+
+fun categorizeOneJoker(input: String): Int {
+    var chunks = input.split(' ')
+    val letterCountMap = mutableMapOf<Char, Int>()
+    letterCountMap += mapOf('A' to chunks.first().count { it == 'A' })
+    letterCountMap += mapOf('K' to chunks.first().count { it == 'K' })
+    letterCountMap += mapOf('Q' to chunks.first().count { it == 'Q' })
+    letterCountMap += mapOf('T' to chunks.first().count { it == 'T' })
+    letterCountMap += mapOf('9' to chunks.first().count { it == '9' })
+    letterCountMap += mapOf('8' to chunks.first().count { it == '8' })
+    letterCountMap += mapOf('7' to chunks.first().count { it == '7' })
+    letterCountMap += mapOf('6' to chunks.first().count { it == '6' })
+    letterCountMap += mapOf('5' to chunks.first().count { it == '5' })
+    letterCountMap += mapOf('4' to chunks.first().count { it == '4' })
+    letterCountMap += mapOf('3' to chunks.first().count { it == '3' })
+    letterCountMap += mapOf('2' to chunks.first().count { it == '2' })
+    val maxChar = letterCountMap.maxBy { it.value }
+    val lel = chunks.first().replace('J', maxChar.key)
+    return when {
+        isFive(lel) -> 6
+        isFour(lel) -> 5
+        isFullHouse(lel) -> 4
+        isThree(lel) -> 3
+        isTwoPair(lel) -> 2
+        isOnePair(lel) -> 1
+        else -> 0
+    }
 }
