d2173bcce3fa904ebf4057977a3ba7fd0f315667
hanna3-14 <114485436+hanna3-14@users.noreply.github.com>
2023-12-11 15:10:20 +0100

    day11: expand space

---
+++ src/Day11.kt
@@ -0,0 +1,80 @@
+fun main() {
+
+    fun part1(input: List<String>): Int {
+
+        val galaxies = mutableListOf<Pair<Int, Int>>()
+        input.forEachIndexed { index, s ->
+            if (s.contains('#')) {
+                val indices = Regex("#").findAll(s).map { it.range.first }.toList()
+                indices.forEach { galaxies.add(Pair(index, it)) }
+            }
+        }
+
+        // expand space from top to bottom
+        val emptyLines = mutableListOf<Int>()
+        for (i in galaxies.indices) {
+            val line = galaxies.filter { it.first == i }
+            if (line.isEmpty()) {
+                emptyLines.add(i)
+            }
+        }
+
+        val newGalaxies = mutableListOf<Pair<Int, Int>>()
+
+        var offset = 0
+        for (line in emptyLines) {
+            for (galaxy in galaxies) {
+                if (line + offset < galaxy.first) {
+                    val gal = galaxy.copy(first = galaxy.first + 1)
+                    newGalaxies.add(gal)
+                }
+            }
+            galaxies.removeIf { line + offset < it.first }
+            newGalaxies.forEach { galaxies.add(it) }
+            newGalaxies.clear()
+            offset += 1
+        }
+
+        // expand space from left to right
+        val emptyColumns = mutableListOf<Int>()
+        for (i in galaxies.indices) {
+            val column = galaxies.filter { it.second == i }
+            if (column.isEmpty()) {
+                emptyColumns.add(i)
+            }
+        }
+
+        offset = 0
+        for (column in emptyColumns) {
+            for (galaxy in galaxies) {
+                if (column + offset < galaxy.second) {
+                    val gal = galaxy.copy(second = galaxy.second + 1)
+                    newGalaxies.add(gal)
+                }
+            }
+            galaxies.removeIf { column + offset < it.second }
+            newGalaxies.forEach { galaxies.add(it) }
+            newGalaxies.clear()
+            offset += 1
+        }
+        println("galaxies: $galaxies")
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day11_1_test")
+    check(part1(testInput) == 374)
+    // check(part2(testInput) == 2)
+
+    val input = readInput("Day11")
+    part1(input).println()
+    part2(input).println()
+
+    // check after submitting the solution
+    //check(part1(input) == 1647269739)
+    // check(part2(input) == 864)
+}
